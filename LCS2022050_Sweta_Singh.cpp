dc25c89ff11147424a09b07d91bf432ec7062b89
swetasingh8844 <120044039+swetasingh8844@users.noreply.github.com>
2023-02-07 00:49:14 +0530

    Add files via upload

---
+++ LCS2022050_Sweta_Singh.cpp
@@ -0,0 +1,7 @@
+Hi everyone
+I am sweta 
+I am from Lucknow 
+I am student at the Indian Institute of Information Technology, Lucknow
+My roll no. is LCS2022050
+I am hard-working girl.
+I love to spend time with my family.
